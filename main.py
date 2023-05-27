4e2a94c4c6d8ac6f65c791fdb0e4572ccad1d9c8
Eleanor <93226333+PeanutNotCashew@users.noreply.github.com>
2023-05-27 14:03:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 apiKey = '902f8009375f89ba96ab427769c38f90'
 
 def createCustomer():
-	url = 'http://api.reimaginebanking.com/customers?key={}'.format(apiKey)
+	url = 'http://api.nessieisreal.com/customers?key={}'.format(apiKey)
 	address = {
 		"street_number":"8000",
 		"street_name":"Utopia Pkwy",
@@ -27,4 +27,15 @@ def createCustomer():
 		print('account created')
 		print(response)
 
+def getCustomers():
+  url = 'http://api.nessieisreal.com/enterprise/customers?key={}'.format(apiKey)
+
+  response = requests.post(url)
+  if response.status_code == 201:
+	  print('got customers')
+  else:
+	  print('got customers fail')
+  print(response)
+
 createCustomer()
+getCustomers()
