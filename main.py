2d115f4246ac2111712e2378cbf571e9c79e925c
Eleanor <93226333+PeanutNotCashew@users.noreply.github.com>
2023-05-27 13:49:02 -0400

    Changed to python

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+apiKey = '902f8009375f89ba96ab427769c38f90'
+
+def createCustomer():
+	url = 'http://api.reimaginebanking.com/customers?key={}'.format(apiKey)
+	address = {
+		"street_number":"8000",
+		"street_name":"Utopia Pkwy",
+		"city":"Jamaica",
+		"state":"NY",
+		"zip":"11372"
+	}
+	customer = {
+		"first_name":"John",
+		"last_name":"Doe",
+		"address":address
+	}
+	response = requests.post(
+		url,
+		data = json.dumps(customer),
+		headers = {'content-type':'application/json'}
+	)
+	
+	if response.status_code == 201:
+		print('account created')
+		print(response)
+
+createCustomer()
