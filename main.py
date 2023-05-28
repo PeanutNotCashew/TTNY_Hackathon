05ec64ad4cda13c6f5dba23c5425661850746b53
Eleanor <93226333+PeanutNotCashew@users.noreply.github.com>
2023-05-27 20:23:56 -0400

    Add selectMerchant method

--- main.py
+++ main.py
@@ -20,11 +20,39 @@ def selectAccount(user):
 
 	return user.accountList[j - 1]
 
+def selectMerchant()
+	j = 1
+	for i in methods.merchantList:
+		print(str(j) + ". " + i.nickname)
+		j += 1
+
+	j = int(input("Input number of merchant: "))
+
+	return methods.merchantList[j - 1]
+
+def accountActions(account):
+	print("1. Make a purchase\n2. Make a deposit\n3. Make a withdrawal")
+	i = int(input("Input number of action: "))
+
+	if i == 1:
+		merchant = selectMerchant()
+		amount = int(input("Amount Spent: "))
+		account.pushPurchase(merchant.getID(), amount)
+	elif i == 2:
+		amount = int(input("Amount Depositing: "))
+		account.pushDeposit(amount)
+	elif i == 3:
+		amount = int(input("Amount withdrawing: "))
+		account.pushWithdrawal(amount)
+
 # Main Code
 methods.createCustomer("John", "Doe")
 methods.customerList[0].addAccount("Credit Card", "Test Credit")
+methods.createMerchant("H&M", "Clothing")
 
-currentAccount = selectAccount(selectCustomer())
+currentUser = selectCustomer()
+currentAccount = selectAccount(currentUser)
+accountActions(currentAccount)
 print(currentAccount)
 
 # Clears data
